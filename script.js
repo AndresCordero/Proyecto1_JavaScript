9264fc6e131259c8b7781108e6210d85fbbadb2f
AndresCordero <an.cornmo@gmail.com>
2024-10-26 19:23:07 -0600

    Inicio de proyeto

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 
-console.log("El mundo es pequeño")
 
-prompt("Ingrese la cantidad de articulos que desea comprar")
+let mainOption = Number(prompt("Seleccione una opcion para iniciar: \n1. Realizar pedido \n2. Salir"))
+if (mainOption === 1) {
+    
+    
+    let amount = prompt("Ingrese la cantidad de articulos que desea comprar")
+    
+    for (let i = 0; i < amount; i++) {
+        let product = prompt("Ingrese el valor del articulo")
+        let product 
+    }    
+    alert("El valor de los articulos es " + total)
+}
+if (mainOption === 2) {
+}
 
 
